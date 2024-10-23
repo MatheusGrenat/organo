e56cfd7b37c633da3285809167a47207c6dad9ec
MatheusGrenat <matheusgrenat@yahoo.com.br>
2024-10-23 20:14:38 -0300

    Concertado o Bug do Card ter descentralizado os textos

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -6,7 +6,7 @@ const Colaborador = ({nome, imagem, cargo }) => {
             <div className='cabecalho'>
                 <img src={imagem} alt='imagem do pai'/>
             </div>
-            <div className='rodape'>
+            <div className='colaborador__rodape'>
                 <h4>{nome}</h4>
                 <h5>{cargo}</h5>
             </div>
